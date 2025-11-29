cbbe122cf17301c8d74d72e780e0d8cb0611cfa6
holecekp <24872113+holecekp@users.noreply.github.com>
2025-11-29 21:00:23 +0100

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiBugPopupShadow
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
